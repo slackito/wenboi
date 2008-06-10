cedc73ece6e61d5adf369ee09f4165885a22de1d
slack <slack@Blackbird.(none)>
2008-06-10 22:48:14 +0200

    Añadido GBVideo.cc

---
+++ GBVideo.cc
@@ -0,0 +1,19 @@
+#include "GBVideo.h"
+
+u8   GBVideo::read_VRAM (int addr) const
+{
+}
+
+u8   GBVideo::read_OAM  (int addr) const
+{
+}
+
+void GBVideo::write_VRAM(int addr, u8 value)
+{
+}
+
+void GBVideo::write_OAM (int addr, u8 value)
+{
+}
+
+
